95b21233b2ad312b72d5eb8badd7bf370ef65048
ArabsWorld <exerror166@gmail.com>
2024-08-12 00:48:34 +0300

    Update config.js

--- boodyhsn/config.js
+++ boodyhsn/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "MTI2OTk5NDc5NjA4OTYwNjIzNQ.Gcsyr5.be5cUPmkG1ev_PdiQf7k_ugPt7NVDYDduNsbxc",
-    guildId: "1262055464255094824",
-    verifyRole: "1269991356231848027",
-    verifyChannel: "1269994079954141205",
+    token: "TOKEN_BOT",
+    guildId: "SERVER_ID",
+    verifyRole: "GIVE_ROLE",
+    verifyChannel: "CHANNEL_ID",
 };
